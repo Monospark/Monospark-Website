370e48ec2ab5f5438bd210f6bc0dc447486b1dfe
Christopher Schnick <c.schnick@monospark.org>
2015-07-25 02:21:07 +0200

    Finish contact page

--- src/contact.php
+++ src/contact.php
@@ -8,9 +8,49 @@
 ?>
 
 <style>
+
   p {
-    background: #123456;
+    color: #444;
   }
+  
+  h1 {
+    color: #3366cc !important;
+    font-size: 34px !important;
+  }
+  
+  hr {
+    border-color: #3366cc !important;
+  }
+
+  .row {
+    padding-top: 1.5em;
+  }
+  
+  .fa-envelope {
+    vertical-align: 8% !important;
+  }
+
+  #contact-monospark {
+    padding-top: 2em;
+	padding-bottom: 4.2em;
+  }
+
+  #contact-monospark p {
+    margin-bottom: 2em !important;
+  }
+  
+  #contact-form {
+    padding-right: 3em;
+  }
+
+  div.form-group {
+    margin-bottom: 1.5em;
+  }
+
+  textarea {
+    resize: none;
+  }
+
 </style>
 
 <?php
@@ -22,9 +62,47 @@
   include "header.php"
 ?>
 
-<p>
-  Hey!
-</p>
+<main>
+
+<section class="container" id="contact-monospark">
+  <h1 class="text-center">Contact Monospark</h1>
+  <hr />
+  <div class="row">
+    <div class="col-md-7" id="contact-form">
+      <form>
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" class="form-control" id="name">
+        </div>
+        <div class="form-group">
+          <label for="email">Email Address</label>
+          <input type="email" class="form-control" id="email">
+        </div>
+        <div class="form-group">
+          <label for="subject">Subject</label>
+          <input type="text" class="form-control" id="subject">
+        </div>
+        <div class="form-group">
+          <label for="message">Message</label>
+          <textarea class="form-control" rows="14" id="message"></textarea>
+        </div>
+		<div class="form-group">
+          <button type="submit" class="btn btn-lg btn-primary"><i class="fa fa-envelope"></i> Submit</button>
+        </div>
+      </form>
+	</div>
+    <div class="col-md-5">
+	  <h4>Project specific questions</h4>
+	  <p>Need some help with a specific Monospark project? You can find help at the Monospark forums (link).</p>
+	  <h4>Project specific issues</h4>
+	  <p>Want to report an issue of a specific Monospark project? You can report issues at the issue tracker of the project.</p>
+	  <h4>Imprint</h4>
+	  <p>Are you looking for the imprint of this website? You can view it here (link)</p>
+	</div>
+  </div>
+</section>
+  
+</main>
 
 <?php
   include "footer.php"
